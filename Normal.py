ee04cbecc906197877713ac50e74bd7ab39d76c3
Agneet Chatterjee <agneet257@gmail.com>
2017-02-18 21:00:00 +0530

    Update Normal.py

--- Normal.py
+++ Normal.py
@@ -4,6 +4,6 @@ img = cv.imread('path')
 img = cv.resize(img, (800,800))
 cv.normalize(img, img, 0, 255, cv.NORM_MINMAX)
 
-cv.imwrite("img0101.bmp",img)
+cv.imwrite("filename",img)
 cv.waitKey(0)
 cv.destroyAllWindows()
